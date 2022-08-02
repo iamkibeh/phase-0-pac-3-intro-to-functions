f16d1e4d8e2442d6847067b3161e1a06a197d3cd
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-08-02 12:52:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Follow along with the examples here
+let namesArr = ["Liz","Guadalupa", "Samip"];
+function sayHello(names) {
+    for (let i = 0; i< names.length; i++) {
+        const element = names[i];
+        console.log(`Hello! ${element}`);
+    }
+    
+  }
+  sayHello(namesArr);
+  function add(x, y) {
+    return x + y;
+  }
+
+  console.log(add(10 , 7))
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+
+  console.log(say("Hello", "Liz")); 
